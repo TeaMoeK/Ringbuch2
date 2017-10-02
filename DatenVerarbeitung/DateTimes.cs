3a4a8e91dbfb5424e9a26e787e7c7d338a599739
Stormtrooper 4711 <lsborg@lanasutra.de>
2017-10-02 16:13:02 +0200

    Add files via upload

---
+++ DatenVerarbeitung/DateTimes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBii.DatenVerarbeitung
+{
+  static class DateTimes
+  {
+    static public int GetAlter(DateTime Geburtstag)
+    {
+      Age age = new Age(Geburtstag);
+      
+      return age.Years;
+    }
+    static public int GetAlterAtSchFest(DateTime Geburtstag)
+    {
+      RBDataVerschiedenesTableAdapters.VerschiedenesTableAdapter adapter = new RBDataVerschiedenesTableAdapters.VerschiedenesTableAdapter();
+      DateTime SchFest = (DateTime)adapter.GetSchFest();
+      Age age = new Age(Geburtstag, SchFest);
+
+      return age.Years;
+    }
+  }
+}
---
+++ Dateneingeben/Personen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace RBii.Dateneingeben
+{
+  class Personen
+  {
+    public void AddNewPerson(DataRow dr)
+    {
+
+    }
+    public void AddNewPerson()
+    {
+
+    }
+    public void UpdatePerson(DataRow dr)
+    {
+
+    }
+    public void UpdatePerson()
+    {
+
+    }
+  }
+}
